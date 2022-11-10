127f1ba3782598dc16a5f7aefdfb2d84948de15b
dressStreet <67256771+Lamzang@users.noreply.github.com>
2022-11-11 02:31:47 +0900

    neighborhood post commit
    동네생활 중 post부분의 기초뼈대를 만들었습니다

--- packages/carrot-app/src/components/neighborhood/popularpost/index.tsx
+++ packages/carrot-app/src/components/neighborhood/popularpost/index.tsx
@@ -1,4 +1,11 @@
+import styled from "styled-components";
+
+const popularListData = [];
+
 const PopularPost = () => {
-  return <div>PopularPost</div>;
+  return <PopularContainer>popularList</PopularContainer>;
 };
 export default PopularPost;
+const PopularContainer = styled.div`
+  width: 100%;
+`;
