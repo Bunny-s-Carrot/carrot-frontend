dcf77b5007706c591e07a53734c9d6a929818152
teamglue-jun <jun@teamglue.io>
2023-01-29 22:50:04 +0900

    feat: 상태바 높이만큼 padding

--- packages/carrot-core/style/globalStyle.tsx
+++ packages/carrot-core/style/globalStyle.tsx
@@ -30,7 +30,7 @@ body{
     font-family: Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif;
     color: #323232;
     margin:0;
-    padding:0;
+    padding: 1.2rem 0 0 0;
     overflow: hidden;
     overscroll-behavior:contain;
     #App { 
