b3dd1411de7fdb68338d0d64865a035f0b8a7eb7
t-junne <tjunnne@gmail.com>
2023-02-03 14:47:20 +0900

    feat: javascriptHandler

--- packages/carrot-app/src/pages/around/index.tsx
+++ packages/carrot-app/src/pages/around/index.tsx
@@ -1,11 +1,31 @@
 import styled from "styled-components";
 import FloatingButton from "../../components/floatingButton";
 import NavBar from "../../components/navBar"
+import { useEffect, useState } from "react";
+import Button from "@carrot/core/atoms/button";
 
 const AroundPage = () => {
+  const [isAppReady, setIsAppReady] = useState(false)
+  useEffect(() => {
+    window.addEventListener('flutterInAppWebViewPlatformReady', () => {
+      setIsAppReady(true);
+    })
+  })
 
+  const handleClickInput = () => {
+    if (!isAppReady) {
+      window.flutter_inappwebview.callHandler('keyboardHeight')
+        .then((res: any) => alert(JSON.stringify(res)))
+    }
+  }
   return (
     <Wrapper>
+      <StyledButton
+        buttonType="CARROT"
+        onClick={handleClickInput}
+      >
+        테스트
+      </StyledButton>
       <FloatingButton pageType="AROUND" />
       <NavBar pageType="AROUND" />
     </Wrapper>
@@ -17,4 +37,10 @@ export default AroundPage;
 
 const Wrapper = styled.div`
 
+`
+
+const StyledButton = styled(Button)`
+position: absolute;
+left: 50%;
+bottom: 50%;
 `
