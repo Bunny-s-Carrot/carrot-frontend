3c55ad9c3b62ec445c38fd01f821e56619bbb0ba
qkrwhdgh126 <68217732+qkrwhdgh126@users.noreply.github.com>
2023-02-06 10:35:58 +0900

    fix: 코드 수정

--- packages/carrot-app/src/pages/neighborhood/[post_id]/[post_id].viewModel.ts
+++ packages/carrot-app/src/pages/neighborhood/[post_id]/[post_id].viewModel.ts
@@ -9,15 +9,14 @@ import useJwtDecode from '../../../hooks/auth/useJwtDecode';
 const usePostDetailViewModel = () => {
   const params = useParams<{ post_id: string, comment_id: string }>();
 
-  const { data } = useQuery(['post', params.post_id], () => 
-    postApi.getPostDetail(params.post_id!));
-
-  const { data: ImageData, isSuccess: getImageSuccess } = useQuery(['post/image', params.post_id], () => 
-  postApi.getImageList(params.post_id!))
-
   const [ content, setContent ] = useState("");
   const { getId } = useJwtDecode();
   const writer_id = getId();
+  const { data } = useQuery(['post', params.post_id], () => 
+    postApi.getPostDetail(params.post_id!, writer_id));
+
+  const { data: ImageData, isSuccess: getImageSuccess } = useQuery(['post/image', params.post_id], () => 
+  postApi.getImageList(params.post_id!))
   const post_id = parseInt(params.post_id!)
   const mother_id = parseInt(params.comment_id!)
   
